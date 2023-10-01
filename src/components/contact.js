2de0e31a1686da18adf9e3703dca665b2a4f5304
Kirandeep <kirandeep1195@gmail.com>
2023-10-01 00:34:03 -0400

    applied form check

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,57 +1,57 @@
 import React from 'react';
-import  { useState } from 'react';
+import { useState } from 'react';
 
 import '../styles/contact.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
- // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
+// Create state variables for the fields in the form
+// We are also setting their initial values to an empty string
 
-  function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
- const Contact = ()=> {
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+const Contact = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [messageVisible, setMessageVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [messageVisible, setMessageVisible] = useState(true);
 
-  const handleChange = (e)=>{
-const inputName = e.target.name;
-const inputValue = e.target.value;
-console.log(`inputName: ${inputName}, inputValue: ${inputValue}`);
- // Based on the input type, we set the state of either email,name,message
- if(inputName === 'name'){
-  setName(inputValue);
- }else if(inputName === 'email'){
-  setEmail(inputValue);
- }else if (inputName ==='message'){
-  setMessage(inputValue)
- }
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    console.log(`inputName: ${inputName}, inputValue: ${inputValue}`);
+    // Based on the input type, we set the state of either email,name,message
+    if (inputName === 'name') {
+      setName(inputValue);
+    } else if (inputName === 'email') {
+      setEmail(inputValue);
+    } else if (inputName === 'message') {
+      setMessage(inputValue)
+    }
   };
 
 
- const handleformSubmit = (e) => {
-e.preventDefault();
- 
-  if(!validateEmail(email)){
-    setErrorMessage('enter valid email')
-    return;
-  };
-  if(name.length < 5){
-setErrorMessage('Enter 5 or more characters for name')
-return;
-  };
-if( message.length < 5){
-  setErrorMessage('Enter proper Message');
-  return;
-}
+  const handleformSubmit = (e) => {
+    e.preventDefault();
 
-setErrorMessage('Form has been submitted successfully');
-setMessageVisible(true);
+    if (!validateEmail(email)) {
+      setErrorMessage('enter valid email')
+      return;
+    };
+    if (name.length < 5) {
+      setErrorMessage('Enter 5 or more characters for name')
+      return;
+    };
+    if (message.length < 5) {
+      setErrorMessage('Enter proper Message');
+      return;
+    }
+
+    setErrorMessage('Form has been submitted successfully');
+    setMessageVisible(true);
 
     // Clear the message after 3 seconds
     setTimeout(() => {
@@ -59,53 +59,53 @@ setMessageVisible(true);
       setMessageVisible(false);
     }, 2000);
 
-  setName('');
-  setEmail('');
-  setMessage('');
- };
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
 
 
-  
+
 
   return (
     <div>
-    <Form>
-       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Name</Form.Label>
-        <Form.Control value={name}
-          name="name"
-          onChange={handleChange} type="text" placeholder="Name" />
-      </Form.Group>
-     
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control   value={email}
-          name="email" type="email"  onChange={handleChange} placeholder="Enter email" />
-        
-      </Form.Group>
+      <Form onSubmit={handleformSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Name</Form.Label>
+          <Form.Control value={name}
+            name="name"
+            onChange={handleChange} type="text" placeholder="Name"  />
+        </Form.Group>
 
-     
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Message</Form.Label>
-        <Form.Control value={message}
-          name="message"
-          onChange={handleChange} type="text" placeholder="" />
-      </Form.Group>
-       
-     
-      <Button variant="primary" onClick={handleformSubmit} type="submit">
-        Submit
-       
-      </Button>
-      {(errorMessage && messageVisible) && (
-      <div>
-      <p className='error'>{errorMessage}</p></div>
-    )}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control value={email}
+            name="email" type="email" onChange={handleChange} placeholder="Enter email" />
+
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Message</Form.Label>
+          <Form.Control value={message}
+            name="message"
+            onChange={handleChange} type="text" placeholder=""  />
+        </Form.Group>
+
+
+        <Button variant="primary" type="submit">
+          Submit
+
+        </Button>
+        {(errorMessage && messageVisible) ? (
+          <div>
+            <p className='error'>{errorMessage}</p></div>
+        ) : ""}
       </Form>
-     
+
     </div>
-   
-   
+
+
   )
- }
- export default Contact;
+}
+export default Contact;
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,12 +1,9 @@
 'use client'
 import React from 'react';
-import cheatsheetImage from '../assets/cheatsheet.png';
-import mediabrowserImage from '../assets/mediabrowser.png';
-import notesImage from '../assets/notes.png';
-import weatherImage from '../assets/weather.png';
-import quizImage from '../assets/quiz.png';
+
 import '../styles/portfolio.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import projectdata from '../assets/Projectdata.json'
 
 import { bounce } from 'react-animations';
 
@@ -21,43 +18,49 @@ const styles = {
   return (
  
 <div className='container'>
-      <div className='topimg'>
-        <div className='imgcontainer'>
-          <img className="test" style={styles.bounce} src= { cheatsheetImage}alt='cheatsheet' />
-        <p>Cheatsheet</p>
-           <button className="repo-button"><a  href="https://github.com/Teelsam/CheatSheetGen">Repository</a></button>
-           <button className="deploy-button"><a href="https://obscure-stream-33374-4c4296c6212a.herokuapp.com/">Deploy</a></button>
-         </div>
-       
-         <div className='imgcontainer'>
-        <img src= {mediabrowserImage} alt='movie' />
-          <p>Movie Browser</p>
-          <button className="repo-button"><a href="https://github.com/99Anvar99/Media-Universe">Repository</a></button>
-           <button className="deploy-button"><a href=" https://99anvar99.github.io/Media-Universe/">Deploy</a></button>
-          </div>
-          <div className='imgcontainer'>
-          <img src= {notesImage} alt='schedule'  />
-          <p>Work Day Scheduler</p>
-          <button className="repo-button"><a href="https://github.com/MK0999/almanac">Repository</a></button>
-           <button className="deploy-button"><a href="https://mk0999.github.io/almanac/">Deploy</a></button>
-          </div>
-       </div>
-       <div className='bottomimg'>
-       <div className='imgcontainer'>
-         <img src={weatherImage} alt='weather' />
-         <p>Weather Forecast</p>
-          <button className="repo-button"><a href="https://github.com/MK0999/shriveled">Repository</a></button>
-           <button className="deploy-button"> <a href="https://mk0999.github.io/shriveled/">Deploy</a></button>
-          </div>
-          <div className='imgcontainer'>
-           <img src={quizImage} alt='quiz' />
-           <p>Quiz</p>
-          <button className="repo-button"><a href="https://github.com/MK0999/protestation">Repository</a></button>
-           <button className="deploy-button"><a href="https://mk0999.github.io/protestation/">Deploy</a></button>
-          </div>
-        </div>
+      <div className='topimg '>
+        {
+          projectdata.map(project => {
+            return (
+              <div className='imgcontainer col-3'>
+              <img className="test" style={styles.bounce} src= {project.imgSrc}alt='cheatsheet' />
+            <p>{project.ProjectTitle}</p>
+               <button className="repo-button"><a  href={project.repoLink}>Repository</a></button>
+               <button className="deploy-button"><a href={project.deployed}>Deploy</a></button>
+             </div>
+            
+             )
+            })
+          }
+            </div>
+             
+        
+        
+        
     </div>
     
   )
  }
  export default Project
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
